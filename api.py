46b9639f4a30bf3dce686f839bb0c6f8de7fcbfa
Sanket Saurav <sanketsaurav@gmail.com>
2014-01-19 01:23:00 +0530

    Added views.

--- api.py
+++ api.py
@@ -1,11 +1,29 @@
-from flask import Flask, jsonify
-import requests
+from flask import Flask, jsonify, redirect, render_template
+from utils import fetch_page, extract_meta, extract_meta_prop
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-	return "Hello, world"
+def homepage():
+	return render_template('index.html')
+
+@app.route('/meta/', methods=['GET'])
+def meta_redirect():
+	return redirect('/')
+
+@app.route('/meta/<path:url>', methods=['GET'])
+def meta(url=None):
+	if not url.startswith('http'):
+		url = 'http://' + url
+	response, status = extract_meta(fetch_page(url))
+	return jsonify({'status' : status, 'data' : response})
+
+@app.route('/meta/<prop>/<path:url>', methods=['GET'])
+def meta_prop(prop=None, url=None):
+	if not url.startswith('http'):
+		url = 'http://' + url
+	response, status = extract_meta_prop(fetch_page(url), prop)
+	return jsonify({'status' : status, 'data' : response})
 
 if __name__ == '__main__':
 	app.run(debug=True)
